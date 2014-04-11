262d8e32a6a0e21b370655f887ea26ca3175f658
Chiwan Park <chiwanpark@icloud.com>
2014-04-12 00:38:43 +0900

    Add code for unit test

---
+++ GAEChannelTestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GAEChannelTestApp
+//
+//  Created by Chiwan Park on 4/12/14.
+//  Copyright (c) 2014 Chiwan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- GAEChannelTests/GAEChannelTests.m
+++ GAEChannelTests/GAEChannelTests.m
@@ -7,28 +7,67 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "GAEChannel.h"
+#import "AFNetworking.h"
+#import "XCTestCase+AsyncTesting.h"
 
-@interface GAEChannelTests : XCTestCase
-
+@interface GAEChannelTests : XCTestCase <GAEChannelDelegate> {
+  NSString *baseURL;
+  GAEChannel *channel;
+  AFHTTPRequestOperationManager *manager;
+  NSDictionary *params;
+}
 @end
 
 @implementation GAEChannelTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)setUp {
+  [super setUp];
+
+  baseURL = @"http://localhost:8080";
+  manager = [AFHTTPRequestOperationManager manager];
+  params = @{@"client_id": [[NSUUID UUID] UUIDString]};
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)tearDown {
+  [self waitForStatus:XCTAsyncTestCaseStatusSucceeded timeout:20];
+  [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testBasic {
+  NSString *url = [NSString stringWithFormat:@"%@/open-channel", baseURL];
+
+  [manager POST:url
+     parameters:params
+        success:^(AFHTTPRequestOperation *operation, id responseObject) {
+          NSString *token = [((NSDictionary *) responseObject) objectForKey:@"token"];
+          channel = [GAEChannel channelWithServerURL:baseURL Delegate:self];
+          [channel connectWithToken:token];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    NSLog(@"%s: failed on token acquiring", __PRETTY_FUNCTION__);
+    [self notify:XCTAsyncTestCaseStatusFailed];
+  }];
 }
+
+- (void)channelInitialized:(GAEChannel *)ignored {
+  NSLog(@"Google Channel API is initialized");
+}
+
+- (void)onOpen {
+  NSLog(@"GAEChannel Opened");
+}
+
+- (void)onMessage:(NSString *)message {
+  NSLog(@"Message Arrived: %@", message);
+}
+
+- (void)onError:(NSInteger)code WithDescription:(NSString *)description {
+  NSLog(@"Error occured: %ld, %@", (long) code, description);
+}
+
+- (void)onClose {
+  NSLog(@"GAEChannel Closed!");
+}
+
 
 @end
